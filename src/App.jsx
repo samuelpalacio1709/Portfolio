29ce4ee33ec1ea193ba55e913e90ba41ab38798d
Samuel Palacio <82546723+samuelpalacio1709@users.noreply.github.com>
2023-11-17 18:19:30 -0500

    Video Player

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import './App.css';
 import { Header } from './components/Header';
 import { Card } from './components/Card';
 import { info } from './information';
+import { VideoPlayer } from './components/VideoPlayer';
 
 function App() {
   return (
     <>
       <Header />
-      {info.map((item, index) => {
+      {/* {info.map((item, index) => {
         return <Card key={index} project={item} />;
-      })}
+      })} */}
     </>
   );
 }
